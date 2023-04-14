a805a007cadc455f510d4dc202e6191c165ccce4
Enoch <mmejuenoch@gmail.com>
2023-04-14 18:19:59 +0100

    Base files

---
+++ src/pages/about.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+const about: React.FC = () => {
+  return (
+    <>
+      <Head>
+        <title>Hello about</title>
+      </Head>
+      <div>About page</div>
+    </>
+  );
+};
+
+export default about;
